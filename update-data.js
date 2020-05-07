45fb17bd8cf1adb8b33236f4f7bc25ff742a33ba
Atte Juvonen <atte.juvonen@futurice.com>
2020-05-07 15:22:12 +0300

    Fetch latest Rt estimate PNG at build.

--- update-data.js
+++ update-data.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-function write(filename, content) {
-    fs.writeFile(`data/${filename}`, content, function(err) {
+function write(relativePath, content) {
+    fs.writeFile(relativePath, content, function(err) {
         if(err) {
             console.log(err);
             process.exit(1); // Prevent build if disk write fails
@@ -20,7 +20,7 @@ async function callbackRtEstimate(response) {
         console.log('Last Rt estimate:', lastRtEstimateValue, lastRtEstimateDate)
         if (lastRtEstimateValue > 0 && lastRtEstimateValue < 10) {
             write(
-                'latest_Rt.csv',
+                'data/latest_Rt.csv',
                 `date,Rt\n${lastRtEstimateDate},${lastRtEstimateValue}`
             )
         } else {
@@ -33,17 +33,28 @@ async function callbackRtEstimate(response) {
     });
 }
 
+async function callbackRtPNG(response) {
+    response.buffer()
+    .then((blob) => {
+        write('public/latest_Rt.png', blob)
+    })
+    .catch(error => {
+        console.log(error)
+        process.exit(1); // Prevent build if parse fails
+    });
+}
+
 async function callbackHSConfirmedCasesAndDeaths(response) {
     response.json()
     .then((json) => {
-        write("latest_hs_cases_and_deaths.json", JSON.stringify(json))
+        write("data/latest_hs_cases_and_deaths.json", JSON.stringify(json))
     })
 }
 
 async function callbackHSHospitalizations(response) {
     response.json()
     .then((json) => {
-        write("latest_hs_hospitalizations.json", JSON.stringify(json))
+        write("data/latest_hs_hospitalizations.json", JSON.stringify(json))
     })
 }
 
@@ -66,5 +77,6 @@ async function fetchOrExit(url, callback) {
 }
 
 fetchOrExit("https://coronastoragemyvs.blob.core.windows.net/estimate-rt/latest_Rt.csv", callbackRtEstimate)
+fetchOrExit("https://coronastoragemyvs.blob.core.windows.net/estimate-rt/latest_Rt.png", callbackRtPNG)
 fetchOrExit("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2", callbackHSConfirmedCasesAndDeaths)
 fetchOrExit("https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaHospitalData", callbackHSHospitalizations)
