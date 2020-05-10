17b884dd112e0f333d09152c91dd4cc63d111482
Atte Juvonen <atte.juvonen@futurice.com>
2020-05-10 14:31:18 +0300

    Minor (change default action markers).

--- src/models/gohs_seir_ode.js
+++ src/models/gohs_seir_ode.js
@@ -168,8 +168,8 @@ export function map_goh_states_into_UFStates(goh_states, N, P_ICU) {
 
 export function goh_default_action_markers() {
     return [
-        new ActionMarkerData(93, "Lift restrictions", 1.4),
-        new ActionMarkerData(123, "Heatwave begins", -0.2),
+        new ActionMarkerData(108, "Lift restrictions", 1.4),
+        new ActionMarkerData(136, "Heatwave begins", -0.2),
         new ActionMarkerData(165, "Schools open", 0.13),
     ]
 }
